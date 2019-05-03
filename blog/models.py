5e9aa778f7d085da319920301c7ca40f8bf4cc63
Shreyans Bhatt <shreyans.bhatt@live.com>
2019-05-03 22:24:45 +0530

    Final dev

--- blog/models.py
+++ blog/models.py
@@ -5,3 +5,10 @@ class blog(models.Model):
     pub_date = models.DateField(auto_now=False, auto_now_add=False)
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        summarystring = " ".join(self.body.split()[:2])
+        return summarystring
